e54068d1308328fc89c6f4312904c7e010daeaa4
Sala D1 <CSalaD1@uniboyaca.edu.co>
2026-04-25 10:58:41 -0500

    Implementación de crud en contactos

--- contactos_ms/app/Presentation/Routers/endpoints.php
+++ contactos_ms/app/Presentation/Routers/endpoints.php
@@ -5,15 +5,23 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\App;
 use App\Presentation\Repositories\TestRepository;
 use App\Presentation\Repositories\ContactosRepository;
-
+use Slim\Routing\RouteCollectorProxy;
 
 return function (App $app) {
-    $app->get('/', [TestRepository::class, 'default'] );
-    $app->post('/hola', [TestRepository::class, 'hola'] );
+    $app->get('/', [TestRepository::class, 'default']);
+    $app->post('/hola', [TestRepository::class, 'hola']);
 
     $app->post('/contacto', [ContactosRepository::class, 'create']);
     $app->get('/contactos', [ContactosRepository::class, 'all']);
     $app->get('/contacto/{id}', [ContactosRepository::class, 'detail']);
     $app->put('/contacto/{id}', [ContactosRepository::class, 'update']);
     $app->delete('/contacto/{id}', [ContactosRepository::class, 'delete']);
+
+    $app->group('/contactos-v2', function (RouteCollectorProxy $group) {
+        $group->get('', [ContactosRepository::class, 'all']);
+        $group->get('/{id}', [ContactosRepository::class, 'detail']);
+        $group->post('', [ContactosRepository::class, 'create']);
+        $group->put('/{id}', [ContactosRepository::class, 'update']);
+        $group->delete('/{id}', [ContactosRepository::class, 'delete']);
+    });
 };
